fda4c53ad316aff4d20fcf54625d9da63b98e960
magnus udbjorg <magnus@udbjorg.net>
2016-11-05 15:51:11 +0100

    Enabled service worker

--- _src/app.js
+++ _src/app.js
@@ -6,6 +6,38 @@ import * as search from "./components/search.js"
 import * as tracking from "./components/tracking.js"
 
 tracking.for_google();
-search.init().then((index)=>{
+search.init().then((index) => {
     search.bootstrap_dom("#search-box", "#search-button", search.For);
 });
+
+
+if ('serviceWorker' in navigator) {
+
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // If this fires we should check if there's a new Service Worker
+        // waiting to be activated. If so, ask the user to force refresh.
+        if (registration.waiting && registration.waiting.state !== "installed") {
+            document.location.reload(true);
+            return;
+        }
+
+        // We should also start tracking for any updates to the Service Worker.
+        registration.onupdatefound = function(event) {
+            console.log("A new version has been found... Installing..." + this.state);
+            // If an update is found the spec says that there is a new Service Worker
+            // installing, so we should wait for that to complete then show a
+            // notification to the user.
+            registration.installing.onstatechange = function(event) {
+                if (this.state === 'installed') {
+                    document.location.reload(true);
+                } else {
+                    console.log("New Service Worker state: ", this.state);
+                }
+            };
+        };
+    }, function(err) {
+        console.log(err);
+    });
+} else {
+    console.log("No service worker :(");
+}
---
+++ sw.js
@@ -0,0 +1,81 @@
+﻿/* global self, caches, fetch, URL, Response */
+//require('serviceworker-cache-polyfill');
+var prefix = 'udbjorg';
+
+var version = 1;
+var previousVersion = version - 1;
+var offline_version = "0.0.1";
+
+var CACHE_NAME = `${prefix}-v${version}`;
+var PREVIOUS_CACHE_NAME = `${prefix}-v${previousVersion}`;
+var OFFLINE_PAGE = `/offline/offline-${offline_version}.html`;
+
+var urlsToCache = [
+    "/",
+    "/assets/css/main.css",
+    "/assets/js/app.js"
+];
+var ignoreUrl = function (url) {
+    const uri = new URL(url); 
+    return false;
+};
+
+self.addEventListener('fetch', function (event) {
+
+    if (ignoreUrl(event.request.url) || event.request.method !== 'GET') {
+        return;
+    }
+    
+    var fetchRequest = event.request.clone();
+    var networkRequest = fetch(fetchRequest)
+        .then(
+            function (response) {
+                if (!response || response.status !== 200 || response.type !== 'basic') {
+                    return response;
+                }
+                var responseToCache = response.clone();
+                caches.open(CACHE_NAME)
+                    .then(function (cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+                return response;
+            }
+        );
+    event.respondWith(
+        caches.match(event.request).then(function (response) {
+            return response || networkRequest;
+        })
+        .catch(function () {
+            if (event.request.mode === "navigate") {
+                return caches.match(OFFLINE_PAGE);
+            }
+        })
+    );
+});
+
+self.addEventListener('install', function (event) {
+   
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(function (cache) {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('activate', function (event) {
+    
+    var cacheWhitelist = [CACHE_NAME, PREVIOUS_CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (cacheWhitelist.indexOf(cacheName) === -1) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        }).then(() => self.clients.claim())
+    );
+
+});
