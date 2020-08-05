714a5f3fc4ebb444e449b39855dd373700a520fc
Magnus Udbjørg <mlu@monosolutions.com>
2020-08-05 16:37:22 +0200

    up

--- _src/app.js
+++ _src/app.js
@@ -1,37 +1,31 @@
-require("whatwg-fetch");
-require('babel-core/register');
-require('babel-polyfill');
-
 import * as search from "./components/search.js"
-import * as tracking from "./components/tracking.js"
+console.log("Udbjorg V1.0.3");
 
-console.log("Udbjorg V1.0.2");
-tracking.for_google();
 
 search.bootstrap_dom("#search-box", "#search-button", search.For, search.init());
 
 // @ts-ignore
-window.errorSearch= function(){
+if (window.errorpage) {
     let query = decodeURI(location.pathname);
     let spilt = query.split("/");
     console.log(spilt[spilt.length])
     search.For(spilt[spilt.length], search.init())
-    .then(results=> {
-       console.log(results)
-    })
-    .catch(_ => console.log(_));
+        .then(results => {
+            console.log(results)
+        })
+        .catch(_ => console.log(_));
 }
 
 if ('serviceWorker' in navigator) {
 
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {      
+    navigator.serviceWorker.register('/sw.js').then(function (registration) {
         if (registration.waiting && registration.waiting.state !== "installed") {
             document.location.reload(true);
             return;
         }
-        registration.onupdatefound = function(event) {
+        registration.onupdatefound = function (event) {
             console.log("A new version has been found... Installing..." + this.state);
-            registration.installing.onstatechange = function(event) {
+            registration.installing.onstatechange = function (event) {
                 if (this.state === 'installed') {
                     document.location.reload(true);
                 } else {
@@ -39,7 +33,7 @@ if ('serviceWorker' in navigator) {
                 }
             };
         };
-    }, function(err) {
+    }, function (err) {
         console.log(err);
     });
 } else {
