72c1f556ba32ea3901ebbb503f83b4762f2ea8f8
Magnus Udbjørg <mlu@monosolutions.com>
2020-08-05 15:10:36 +0200

    h

--- _plugins/algolia_hook.rb
+++ _plugins/algolia_hook.rb
@@ -2,7 +2,7 @@ module Jekyll
   module Algolia
     module Hooks
       def self.before_indexing_each(record, node, context)
-        record[:links] = node.to_html
+        record[:links] = node
 
         record
       end
