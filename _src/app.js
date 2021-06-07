d5a1771543d7f333f6d60e71adb829d4834a7e6e
Magnus Udbjørg <mlu@monosolutions.com>
2021-06-07 11:24:13 +0200

    back to sw.js

--- _src/app.js
+++ _src/app.js
@@ -19,7 +19,7 @@ if (window.errorpage) {
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      navigator.serviceWorker.register('/sw.js').then(registration => {
         console.log('SW registered: ', registration);
       }).catch(registrationError => {
         console.log('SW registration failed: ', registrationError);
