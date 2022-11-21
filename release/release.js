3128ca7b83a196fd45ea08d6ea363076faab7999
Magnus Udbjørg <magnus@udbjorg.net>
2022-11-21 10:48:58 +0100

    chore: fix separator

--- release/release.js
+++ release/release.js
@@ -1,18 +1,21 @@
-const { match } = require("assert");
 const path =  require("path")
-const file = process.argv[2];
-const blogFileMatch = file.match(/(\d\d\d\d\-\d\d\-\d\d)/g);
+const files = process.argv[2].split(",");
+const blogFiles = files.filter((file)=>{
+    const filename = path.basename(file, ".md")
+    const match = filename.match(/(\d\d\d\d\-\d\d\-\d\d)/g);
 
-console.log(blogFileMatch);
+    return match;
+})
 
-if(!blogFileMatch && path.extname(file) !== '.md'){
-    console.log("Not a blog post, won't annouce to the world...")
+console.log(blogFiles);
+
+if(blogFiles.length <= 0){
+    console.log("no need to update")
     return
 }
-
-const filename = path.basename(file, ".md")
+const filename = path.basename(blogFiles[0], ".md")
 const title = filename.replace(/(\d\d\d\d\-\d\d\-\d\d)/g, "")
-const dateString = blogFileMatch[0];
+const dateString = filename.match(/(\d\d\d\d\-\d\d\-\d\d)/g)[0];
 
 const date = new Date(dateString);
 const link = `https://udbjorg.net/${date.getFullYear()}/${date.getMonth()}/${title}`
